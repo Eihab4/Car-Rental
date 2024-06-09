f2f036257d10249bbc091061c64585f035275578
Eihab4 <128827403+Eihab4@users.noreply.github.com>
2024-06-10 02:48:30 +0300

    Update specialApi.routes.js

--- modules/specialApi/specialApi.routes.js
+++ modules/specialApi/specialApi.routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import { availableAndRentedCarsWithSpecificModel, availableCarsWithSpecificModel, carsModelsHondaAndToyota, carsRentedOrSpecificModel } from "./specialApi.controller.js";
 
+export const specialApiRouter = Router();
 
-
-export const specialApiRouter = Router()
-
-specialApiRouter.get("/:m1/:m2", carsModelsHondaAndToyota);
-specialApiRouter.get("/availableCarsWithSpecificModel/:model", availableCarsWithSpecificModel);
-specialApiRouter.get("/carsRentedOrSpecificModel/:model", carsRentedOrSpecificModel)
-specialApiRouter.get("/availableAndRentedCarsWithSpecificModel/:model",availableAndRentedCarsWithSpecificModel)
+specialApiRouter.get("/models/:m1/:m2", carsModelsHondaAndToyota);
+specialApiRouter.get("/available/:model", availableCarsWithSpecificModel);
+specialApiRouter.get("/rented-or-specific/:model", carsRentedOrSpecificModel);
+specialApiRouter.get("/available-and-rented/:model", availableAndRentedCarsWithSpecificModel);
